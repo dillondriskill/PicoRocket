b258f21481bcf3722914c7847f161bb9c950609d
platypushunter <dillondriskill2@gmail.com>
2022-12-19 17:45:03 -0800

    initialize project

---
+++ rocket.c
@@ -0,0 +1,22 @@
+/**
+ * @file rocket.c
+ * @author Platypushunter
+ * @brief Guidance system for Pi Pico Powered Rocket 
+ * @version 0.1
+ * @date 2022-12-19
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "pico/binary_info.h"
+
+int main() {
+
+    // TODO: Finish
+
+    return 0;
+}
