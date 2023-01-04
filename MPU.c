479e40b94ab7f6c9a0b331d732568ded89a022d4
platypushunter <dillondriskill2@gmail.com>
2023-01-03 20:43:11 -0500

    this shi wak

--- MPU.c
+++ MPU.c
@@ -10,28 +10,55 @@
  */
 
 #include <stdio.h>
+#include <math.h>
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
 #include "MPU.h"
+#include "math.h"
+
+#define PI 3.14159265359
 
 // By default these devices  are on bus address 0x68
 static int addr = 0x68;
 
-// Calibration offsets
-int16_t AOFF[3] = {-1055, -1078, -1568};
-int16_t GOFF[3] = {-712, 851, -994};
-// int16_t AOFF[3] = {0, 0, 0};
-// int16_t GOFF[3] = {0, 0, 0};
+
+// Calibration offsets - custom to me
+int16_t AOFF[3] = {-1050, -1070, -1560};
+int16_t GOFF[3] = {-710, 850, -990};
+
+/**
+ * @brief Write the data indicated to the mpu
+ * 
+ * @param buf the data
+ * @param len the number of bytes
+ */
+void mpu_write(uint8_t buf[], int len) {
+    i2c_write_blocking(i2c_default, addr, buf, len, false);
+}
+
+/**
+ * @brief Write the data indicated to the mpu, and keep the line open
+ * 
+ * @param buf the data
+ * @param len the number of bytes
+ */
+void mpu_write_c(uint8_t buf[], int len) {
+    i2c_write_blocking(i2c_default, addr, buf, len, true);
+}
 
 /**
  * @brief Reset the mpu to default settings and calibrate the sensors
  * 
  */
 void mpu6050_reset() {
+    uint8_t buf[] = {0x00, 0x00};
 
+    // Reset the mpu, calibrate the sensors, set the sampling rate and enable and reset the FIFO
+    
     // Two byte reset. First byte register, second byte data
-    uint8_t buf[] = {0x6B, 0x00};
-    i2c_write_blocking(i2c_default, addr, buf, 2, false);
+    buf[0] = 0x6B;
+    buf[0] = 0x00;
+    mpu_write(buf, 2);
 
     // Now to calibrate the sensor and set it to 2000 deg/sec and 16g sensitivity
     // Page 14 of register map has this information
@@ -39,18 +66,41 @@ void mpu6050_reset() {
     buf[1] = 0b11111000;  // Options
     // Do this 6 times just to really pump it in
     for (int i = 0; i < 6; i ++) {
-        i2c_write_blocking(i2c_default, addr, buf, 2, false);
+        mpu_write(buf, 2);
     }
 
+    // test gyro
     buf[0] = 0x1C;  // Gyro location
     buf[1] = 0b11111000;  // options
-
     for (int i = 0; i < 6; i ++) {
-        i2c_write_blocking(i2c_default, addr, buf, 2, false);
+        mpu_write(buf, 2);
     }
+
+    /*
+    // Set sampling rate to 8khz / (1 + 7)
+    buf[0] = 0x19;  // Sample rate divider register
+    buf[1] = 0x07;
+    mpu_write(buf, 2);
+    */
+
+    /*
+    // Turn on the fifo for the accelerometer and the gyro
+    buf[0] = 0x23;  // fifo enable reg
+    buf[1] = 0b01111000;
+    mpu_write(buf, 2);
+
+    // Enable and reset the fifo from user control reg
+    buf[0] = 0x6A;  // USER_CTRL
+    buf[1] = 0b01000101;    // Enable and reset FIFO
+    mpu_write(buf, 2);
+    */
 
 }
 
+/**
+ * @brief Calibrates the offsets of the mpu. DO NOT USE!!!!!!
+ * 
+ */
 void mpu_calibrate() {
     int8_t AO[6];   // Accel offset in 8bit bytes
     int8_t GO[6];   // Gyro offset in 8bit bytes
@@ -65,18 +115,17 @@ void mpu_calibrate() {
         GO[i*2] = (GOFF[i] >> 8) & 0xFF;
         GO[i*2+1] = (GOFF[i]) & 0xFF;
     }
-    /*
+    
     // gyro offsets
     uint8_t reg[] = {0x13};
     i2c_write_blocking(i2c_default, addr, reg, 1, true);
     i2c_write_blocking(i2c_default, addr, GO, 6, false);
 
-
     // Accel offsets
     reg[0] = 0x06;
     i2c_write_blocking(i2c_default, addr, reg, 1, true);
     i2c_write_blocking(i2c_default, addr, AO, 6, false);
-    */
+    
 }
 
 void mpu6050_read_raw(int16_t accel[3], int16_t gyro[3]) {
@@ -129,5 +178,136 @@ void mpu_read(double acc[3], double gy[3]) {
             acc[i] = acceleration[i] / 2048.0;
             gy[i] = gyro[i] / 16.4;
         }
+
+}
+
+/**
+ * @brief Reads 1 byte from reg
+ * 
+ * @param reg register to read from 
+ * @return uint8_t 
+ */
+uint8_t mpu_read_reg(uint8_t reg) {
+    uint8_t val;
+
+    mpu_write_c(&reg, 1);
+    i2c_read_blocking(i2c_default, addr, &val, 1, false);
+
+    return val;
+}
+
+/**
+ * @brief Reads len number of bytes from reg into buf
+ * 
+ * @param reg 
+ * @param buf 
+ * @param len 
+ */
+void mpu_read_buf(uint8_t reg, uint8_t buf[], int len) {
+
+    mpu_write_c(&reg, 1);
+    i2c_read_blocking(i2c_default, addr, buf, len, false);
+
+}
+
+/**
+ * @brief Update the position and rotation of the rocket using the MPU FIFO buffer
+ * 
+ * @param x X position
+ * @param y Y position
+ * @param z Z position
+ * @param pitch Pitch
+ * @param yaw Yaw
+ * @param roll Roll
+ */
+void update_postion(double pos[3], double angle[3]) {
+    uint8_t reg1 = 0x72;    // FIFO count reg 1
+    uint8_t reg2 = 0x73;    // FIFO count reg 2
+    uint8_t fifo = 0x74;
+    double acc[3], gy[3];
+    uint8_t buff[6];
+    uint16_t count = ((uint16_t)mpu_read_reg(reg1) << 8) | (uint16_t)mpu_read_reg(reg2);  // Count in FIFO
+
+    
+    // Wait until every sensor has had its bytes written
+    //if (count != 0) {
+
+        // Lets read each byte and put it where it needs to be
+        //while (1) {
+            // Read the fifo 6 times for the acc and then turn that into the 16 bit number for each
+
+            for (int i = 0; i < 6; i++) {
+                buff[i] = mpu_read_reg(fifo);
+            }
+            for (int i = 0; i < 3; i++) {
+                acc[i] = (((int16_t)buff[i*2] << 8) | ((int16_t)buff[i*2+1])) / 2048.0;     // 2048 is the conversion factor here
+            }
+
+            // Read the fifo 6 times for the gy and then turn that into the 16 bit number for each
+            for (int i = 0; i < 6; i++) {
+                buff[i] = mpu_read_reg(fifo);
+            }
+            for (int i = 0; i < 3; i++) {
+                gy[i] = (((int16_t)buff[i*2] << 8) | ((int16_t)buff[i*2+1])) / 16.4;       // 16.4 is the conversion factor here
+            }
+
+            // Update rotational data according to the new data and divide it by the time it took for that reading to happen
+            angle[0] += gy[0] / 1000;
+            angle[1] += gy[1] / 1000;
+            angle[2] += gy[2] / 1000;
+
+
+            // Now to calculate the position. We need to remove the gravitational component of the accelerometer readings
+            acc_cal_grav(acc, angle);
+
+            // Now the acceleration has been calibrated and we can update x y and z
+            pos[0] += acc[0] / 1000;
+            pos[1] += acc[1] / 1000;
+            pos[2] += acc[2] / 1000;
+            
+            // Update the count
+            count = (mpu_read_reg(reg1) << 8) | mpu_read_reg(reg2);
+
+            // Enable and reset the fifo from user control reg
+            uint8_t buf[2];
+            buf[0] = 0x6A;          // USER_CTRL
+            buf[1] = 0b01000101;    // Enable and reset FIFO
+            mpu_write(buf, 2);
+
+        //}
+    //}
+    
+}
+
+/**
+ * @brief Corrects the accelerometer values for gravity
+ * 
+ * @param acc the accelerometer data
+ * @param angle the angle of the rocket
+ */
+void acc_cal_grav(double acc[3], double angle[3]) {
+    double ang_rad[3];                      // Radian version of the angle
+    double grav_ang[3] = {-PI/2, 0, 0};     // The angle of gravity
+    double grav_vec[3];                     // The vector of gravity
+
+    // Convert to radians
+    ang_rad[0] = (angle[0] * PI / 180);
+    ang_rad[1] = (angle[1] * PI / 180);
+    ang_rad[2] = (angle[2] * PI / 180);
+
+    // Add our current angle to the gravity angle to find its relative angle
+    grav_ang[0] += ang_rad[0];
+    grav_ang[1] += ang_rad[1];
+    grav_ang[2] += ang_rad[2];
+
+    // Find the relative gravity vector
+    grav_vec[0] = cos(ang_rad[1]);
+    grav_vec[1] = cos(ang_rad[0]);
+    grav_vec[2] = sin(ang_rad[0]);
+
+    // Subtract gravitational components
+    acc[0] -= grav_vec[0];
+    acc[1] -= grav_vec[1];
+    acc[2] -= grav_vec[2];
 
 }
--- rocket.c
+++ rocket.c
@@ -1,51 +1,233 @@
 /**
- * @file rocket.c
- * @author Platypushunter
- * @brief Pico Powered guided rocket
- * @version 0.1
- * @date 2022-12-21
- * 
- * @copyright Copyright (c) 2022
- * 
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
  */
 
 #include <stdio.h>
+#include <string.h>
 #include "pico/stdlib.h"
+#include "pico/binary_info.h"
 #include "hardware/i2c.h"
-#include "MPU.h"
+#include "math.h"
 
-void i2c_start();
+// By default these devices  are on bus address 0x68
+static int addr = 0x68;
 
-int main() {
-    // Start up all the systems and calibrate the sensors
-    stdio_init_all();
-    i2c_start();
-    mpu6050_reset();
-    mpu_calibrate();
-
-    double acc[3], gy[3];
-
-    while (1) {
-        mpu_read(acc, gy);
-
-        printf("%-5.3f %-5.3f %-5.3f    |    %-5.3f %-5.3f %-5.3f\n", acc[0], acc[1], acc[2], gy[0], gy[1], gy[2]);
-        //printf("Gyro. X = %3f, Y = %3f, Z = %3f\n", gy[0], gy[1], gy[2]);
-        sleep_ms(50);
-    }
-
-    return 0;
-}
+#define PI 3.14159265359
 
 /**
- * @brief Starts up the i2c interface on pins 6 and 7 (gpio 4 and 5) at 400khz
+ * @brief Corrects the accelerometer values for gravity
  * 
+ * @param acc the accelerometer data
+ * @param angle the angle of the rocket
  */
-void i2c_start() {
+void acc_cal_grav(double acc[3], double angle[3]) {
+    /**
+     * If you really think about it... This function should be called frame_shift();
+     *
+     * To correct the accelerometer readings for gravity we need to know the angle that the rocket is facing.
+     * Once we know this angle we can use 3 rotation matrixes (which use a lot of googling to find) to find the correct x y and z components.
+     * I could have figured it out with trig and a lot of thought but I dont have time for that
+     * 
+     * We need to perform the calculation in 3 steps:
+     * 1) Rotate around ROLL
+     * 2) Rotate around PITCH
+     * 3) Rotate around YAW
+     * 
+     * Look up wiki rotation matrix
+     */
 
+    double gravity[3] = {0, 0, -1};      // Vector of gravity on the world plane
+    double rad[3] = {angle[0]*PI/180,     // Euler angle of the rocket in radians
+                     angle[1]*PI/180,
+                     angle[3]*PI/180};
+
+    // Now the actual conversion
+
+ // gravity[0]  Nothing happens this time
+    gravity[1] *= cos(rad[0]) - sin(rad[0]);
+    gravity[2] *= sin(rad[0]) + cos(rad[0]);
+
+    gravity[0] *= cos(rad[1]) + sin(rad[1]);
+ // gravity[1] Nothing happens this time
+    gravity[2] *= -sin(rad[1]) + cos(rad[1]);
+
+    gravity[0] *= cos(rad[2]) - sin(rad[2]);
+    gravity[1] *= sin(rad[2]) + cos(rad[2]);
+ // gravity[2] *= -sin(rad[1]) + cos(rad[1]);
+
+
+    // Now the gravitational vector should be rotated to be pointing in the correct direction so we can simply remove this vector from the acceleration
+    acc[0] -= gravity[0];
+    acc[1] -= gravity[1];
+    acc[2] -= gravity[2];
+
+}
+
+static void mpu6050_reset() {
+    // Two byte reset. First byte register, second byte data
+    // There are a load more options to set up the device in different ways that could be added here
+    uint8_t buf[] = {0x6B, 0x00};
+    i2c_write_blocking(i2c_default, addr, buf, 2, false);
+
+    // Now to calibrate the sensor and set it to 2000 deg/sec and 16g sensitivity
+    // Page 14 of register map has this information
+    buf[0] = 0x1C;  // Accel location
+    buf[1] = 0b11111000;  // Options
+    // Do this 6 times just to really pump it in
+    for (int i = 0; i < 6; i ++) {
+        i2c_write_blocking(i2c_default, addr, buf, 2, false);
+    }
+
+    // test gyro
+    buf[0] = 0x1B;  // Gyro location
+    buf[1] = 0b11111000;  // options
+    for (int i = 0; i < 6; i ++) {
+        i2c_write_blocking(i2c_default, addr, buf, 2, false);
+    }
+
+    // Set sample rate to 1khz
+    buf[0] = 0x19; // SMPRT_DIV
+    buf[1] = 0x07; // 8khz / (1 / SMPRT_DIV)
+    i2c_write_blocking(i2c_default, addr, buf, 2, false);
+
+    // Set low pass filter to ~180
+    buf[0] = 0x1A;          // CONFIG
+    buf[1] = 0b00000001;    // Low pass to 188 hz also sets the gyroscope to 1 khz
+    i2c_write_blocking(i2c_default, addr, buf, 2, false);
+
+    // Enable the FIFO in general and reset it
+    buf[0] = 0x6A;          // USER_CTRL
+    buf[1] = 0b01000100;    // Enable and reset the fifio
+    i2c_write_blocking(i2c_default, addr, buf, 2, false);
+
+    // Enable the FIFO for the accelerometer and the Gyroscope
+    buf[0] = 0x23;          // FIFO_EN
+    buf[1] = 0b01111000;    // Turn on GX GY GZ and all of ACCEL
+    i2c_write_blocking(i2c_default, addr, buf, 2, false);
+
+    // Enable the FIFO in general and reset it
+    buf[0] = 0x6A;          // USER_CTRL
+    buf[1] = 0b01000100;    // Enable and reset the fifio
+    i2c_write_blocking(i2c_default, addr, buf, 2, false);
+
+}
+
+static void mpu6050_read_fifo(int16_t accel[3], int16_t gyro[3]) {
+    uint8_t buffer[6];
+
+    // Start reading acceleration registers from register 0x3B for 6 bytes
+    uint8_t val = 0x3B;
+    i2c_write_blocking(i2c_default, addr, &val, 1, true); // true to keep master control of bus
+    i2c_read_blocking(i2c_default, addr, buffer, 6, false);
+
+    for (int i = 0; i < 3; i++) {
+        accel[i] = (buffer[i * 2] << 8 | buffer[(i * 2) + 1]);
+    }
+
+    // Now gyro data from reg 0x43 for 6 bytes
+    val = 0x43;
+    i2c_write_blocking(i2c_default, addr, &val, 1, true);
+    i2c_read_blocking(i2c_default, addr, buffer, 6, false);  // False - finished with bus
+
+    for (int i = 0; i < 3; i++) {
+        gyro[i] = (buffer[i * 2] << 8 | buffer[(i * 2) + 1]);
+    }
+}
+
+static void mpu6050_read_raw(int16_t accel[3], int16_t gyro[3]) {
+    uint8_t buffer[6];
+
+    // Start reading acceleration registers from register 0x3B for 6 bytes
+    uint8_t val = 0x3B;
+    i2c_write_blocking(i2c_default, addr, &val, 1, true); // true to keep master control of bus
+    i2c_read_blocking(i2c_default, addr, buffer, 6, false);
+
+    for (int i = 0; i < 3; i++) {
+        accel[i] = (buffer[i * 2] << 8 | buffer[(i * 2) + 1]);
+    }
+
+    // Now gyro data from reg 0x43 for 6 bytes
+    val = 0x43;
+    i2c_write_blocking(i2c_default, addr, &val, 1, true);
+    i2c_read_blocking(i2c_default, addr, buffer, 6, false);  // False - finished with bus
+
+    for (int i = 0; i < 3; i++) {
+        gyro[i] = (buffer[i * 2] << 8 | buffer[(i * 2) + 1]);
+    }
+}
+
+void initialize() {
+    // This will get the board ready for flight
+    stdio_init_all();
+
+    // Turn on light cuz why not
+    gpio_pull_up(PICO_DEFAULT_LED_PIN);
+
+    // This will use I2C0 on the default SDA and SCL pins (4, 5 on a Pico)
     i2c_init(i2c_default, 400 * 1000);
     gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
     gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
     gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
     gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
 
+    // Set up the mpu
+    mpu6050_reset();
+}
+
+int main() {
+    initialize();
+
+    int16_t acceleration[3], gyro[3];
+    double acc[3] = {0, 0, 0};
+    double gy[3] = {0, 0, 0};
+    double pos[3] = {0, 0, 0};
+    double angle[3] = {0, 90, 90};
+
+    int16_t AOFF[3] = {-1051, -7, -1525};
+    int16_t GOFF[3] = {-710, 840, -990};
+    uint8_t count_8[2] = {0, 0};
+    uint16_t count;
+
+    sleep_ms(500);
+
+    while (1) {
+
+        // See fifo count
+        uint8_t buf[1] = {0x72};
+        i2c_write_blocking(i2c_default, addr, buf, 1, true);
+        i2c_read_blocking(i2c_default, addr, count_8, 1, false);
+        count = ((uint16_t)count_8[0] << 8) | (uint16_t)count_8[1];
+        
+        mpu6050_read_fifo(acceleration, gyro);
+
+        acc[0] = (((round(acceleration[0] / 10) * 10) + AOFF[0]) / 2048.0);
+        acc[1] = (((round(acceleration[1] / 10) * 10) + AOFF[1]) / 2048.0);
+        acc[2] = (((round(acceleration[2] / 10) * 10) + AOFF[2]) / 2048.0);
+
+        gy[0] = ((round(gyro[0] / 10) * 10) + GOFF[0]) / 16.3835;
+        gy[1] = ((round(gyro[1] / 10) * 10) + GOFF[1]) / 16.3835;
+        gy[2] = ((round(gyro[2] / 10) * 10) + GOFF[2]) / 16.3835;
+
+        // acc_cal_grav(acc, angle);
+
+        angle[0] += gy[0] / 55;
+        angle[1] += gy[1] / 55;
+        angle[2] += gy[2] / 55;
+
+        pos[0] += (acc[0] * 9.8) / 55;
+        pos[1] += (acc[1] * 9.8) / 55;
+        pos[2] += (acc[2] * 9.8) / 55;
+        
+        //printf("Pos. X = %8.3f, Y = %8.3f, Z = %8.3f        ", pos[0], pos[1], pos[2]);
+        //printf("Ang. X = %8.3f, Y = %8.3f, Z = %8.3f        ", angle[0], angle[1], angle[2]);
+        //printf("Fifo = %d\n", count);
+        printf("Acc. X = %8.3f, Y = %8.3f, Z = %8.3f        ", acc[0], acc[1], acc[2]);
+        printf("Gyr. X = %8.3f, Y = %8.3f, Z = %8.3f\n", gy[0], gy[1], gy[2]);
+        sleep_ms(1);
+
+    }
+
+    return 0;
 }
