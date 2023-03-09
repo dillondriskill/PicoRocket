fdab3efbd4fecd6e6e9e8dcba6dfd4ee40717b94
platypushunter <dillondriskill2@gmail.com>
2023-03-09 15:32:45 -0500

    Working on adding terminal commands

--- src/terminal/commands.h
+++ src/terminal/commands.h
@@ -4,9 +4,9 @@
  * @brief Contains the command structs and command list for the terminal program
  * @version 0.1
  * @date 2023-03-08
- * 
+ *
  * @copyright Copyright (c) 2023
- * 
+ *
 */
 
 #pragma once
@@ -14,12 +14,9 @@
 // Number of commands not including the help command. Needs to be updated every time a new command is made
 #define NUMBER_OF_COMMANDS 5
 
-// only used to define where the commands are in the array
-static int commandpos = 0;
-
 /**
  * @brief Struct of a terminal command. Contains the character that calls this command, as well as a pointer to the commands entry point
- * 
+ *
 */
 struct Command {
     char callerchar;
